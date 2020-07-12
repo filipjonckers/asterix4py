79da50e742a6c3ad3ed72eeb14f9dc0bf01bb0e0
filipjonckers <filip@jonckers.org>
2020-07-12 23:30:20 +0200

    packaging

--- setup.py
+++ setup.py
@@ -10,12 +10,30 @@ AUTHOR_EMAIL = 'jof@skeyes.be'
 URL = 'https://github.com/filipjonckers/asterix4py'
 LICENSE = 'MIT License'
 DESCRIPTION = 'Pure python library for decoding Eurocontrol Asterix binary data'
+KEYWORDS = 'asterix radar artas eurocontrol mode-s'
 LONG_DESCRIPTION = (HERE / "README.md").read_text()
 LONG_DESC_TYPE = "text/markdown"
+CLASSIFIERS = [
+      'Development Status :: 3 - Alpha',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Intended Audience :: Developers',
+      'Intended Audience :: Information Technology',
+      'Intended Audience :: Science/Research',
+      'Operating System :: MacOS',
+      'Operating System :: Microsoft :: Windows',
+      'Operating System :: POSIX :: Linux',
+      'License :: OSI Approved :: MIT License',
+      'Topic :: Software Development',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      'Topic :: Scientific/Engineering',
+      'Topic :: Scientific/Engineering :: Information Analysis',
+      'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator'
+]
 
 INSTALL_REQUIRES = [
-      'gzip',
-      'json'
+      # 'xml.dom',
+      # 'json'
 ]
 
 setup(name=PACKAGE_NAME,
@@ -27,7 +45,8 @@ setup(name=PACKAGE_NAME,
       license=LICENSE,
       author_email=AUTHOR_EMAIL,
       url=URL,
+      keywords=KEYWORDS,
+      classifiers=CLASSIFIERS,
       install_requires=INSTALL_REQUIRES,
       packages=find_packages()
       )
-
