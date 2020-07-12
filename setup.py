190f44564e5d7ddd0a92c8cfed714f79a4890c6a
filipjonckers <filip@jonckers.org>
2020-07-12 12:29:20 +0200

    improved python setup

--- setup.py
+++ setup.py
@@ -1,12 +1,33 @@
-from distutils.core import setup
+import pathlib
+from setuptools import setup, find_packages
 
-setup(
-    name='asterix4py',
-    version='1.0.1',
-    packages=['asterix4py'],
-    url='https://github.com/filipjonckers/asterix4py',
-    license='MIT',
-    author='Filip Jonckers',
-    author_email='jof@skeyes.be',
-    description='Pure python library for decoding Eurocontrol Asterix binary data'
-)
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '1.0.1'
+PACKAGE_NAME = 'asterix4py'
+AUTHOR = 'Filip Jonckers'
+AUTHOR_EMAIL = 'jof@skeyes.be'
+URL = 'https://github.com/filipjonckers/asterix4py'
+LICENSE = 'MIT License'
+DESCRIPTION = 'Pure python library for decoding Eurocontrol Asterix binary data'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'gzip',
+      'json'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
+
