578a25e58b72310301b354f2818d81a857f1e9f8
filipjonckers <filip@jonckers.org>
2020-07-12 12:15:37 +0200

    init library components

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='asterix4py',
+    version='1.0.1',
+    packages=['asterix4py'],
+    url='https://github.com/filipjonckers/asterix4py',
+    license='MIT',
+    author='Filip Jonckers',
+    author_email='jof@skeyes.be',
+    description='Pure python library for decoding Eurocontrol Asterix binary data'
+)
