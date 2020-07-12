610a9da4c80462de110b888a5fb2b10d1935faad
filipjonckers <filip@jonckers.org>
2020-07-12 23:44:08 +0200

    cat 48 example added

---
+++ example/ast48.py
@@ -0,0 +1,8 @@
+import asterix4py
+
+b = open('../sample/cat048.ast', 'rb').read()
+print('dec: ', memoryview(b).tolist())
+print('hex: ', ['{:02X}'.format(i) for i in memoryview(b).tolist()])
+
+decoder = asterix4py.AsterixParser(b)
+print('decode: ', decoder.get_result())
