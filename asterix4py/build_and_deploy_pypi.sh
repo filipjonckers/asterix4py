f58c1f19d81b40ee09e3c98f4abe26a056de0b1c
filipjonckers <filip@jonckers.org>
2020-07-12 12:23:02 +0200

    implement PyPi.org build and deploy helper script

---
+++ asterix4py/build_and_deploy_pypi.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+rm -r dist ;
+python setup.py sdist bdist_wheel ;
+if twine check dist/* ; then
+  if [ "$1" = "--test" ] ; then
+    twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+  else
+    twine upload dist/* ;
+  fi
+fi
