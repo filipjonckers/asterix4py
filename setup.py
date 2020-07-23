f3b981fe935476ac1cfa9daa2964cd38d2a26920
filipjonckers <filip@jonckers.org>
2020-07-23 23:39:40 +0200

    released version 1.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 PYTHON_REQUIRES = '>=3.7'
 PACKAGE_NAME = 'asterix4py'
 AUTHOR = 'Filip Jonckers'
