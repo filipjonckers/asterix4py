2fc3bcfe30ea1f674be519c3ce58aeefddddf3ba
filipjonckers <filip@jonckers.org>
2020-07-23 23:37:12 +0200

    improved handling of empty items

--- example/ast62_plotlist_bigfile.py
+++ example/ast62_plotlist_bigfile.py
@@ -1,30 +1,31 @@
 import datetime
+import time
 
 import asterix4py
 
 FILE = '../sample/cat062.ast'
 
 midnight = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+start = time.perf_counter()
 
 with open(FILE, 'rb') as astfile:
     data = astfile.read(3)
     while data:
         cat = data[0]
         length = int.from_bytes(data[1:3], byteorder='big', signed=True)
-        data += astfile.read(length)
-        print('hex: ', ['{:02X}'.format(i) for i in memoryview(data).tolist()])
+        data += astfile.read(length - 3)
+        # print('hex: ', ['{:02X}'.format(i) for i in memoryview(data).tolist()])
         decoder = asterix4py.AsterixParser(data)
-        print('decode: ', decoder.get_result())
+        # print('decode: ', decoder.get_result())
 
-        # each plot has a sequence number
+        # each plot/record of a frame has a sequence number
         for nr, plot in decoder.get_result().items():
             cat = plot.get('cat')
             if cat != 62:
-                print(f"cat{cat:03d}")
                 continue
 
             print(
-                f"cat{cat:03d}"
+                f"{nr:d} cat{cat:03d}"
                 f" tot:{midnight + datetime.timedelta(seconds=plot['070']['ToT']):%H:%M:%S.%f}"
                 , end='')
 
@@ -41,22 +42,25 @@ with open(FILE, 'rb') as astfile:
 
             if plot.get('380'):
                 print(
-                    f" s:{plot['380'].get('ADR'):06X}"
-                    f" acid:{plot['380'].get('ACID')}"
-                    f" fss:{plot['380'].get('FSS')}"
-                    f" gsp:{plot['380'].get('GSP')}"
-                    f" ias:{plot['380'].get('IAS')}"
+                    f" s:{plot['380'].get('ADR', 0):06X}"
+                    f" acid:{plot['380'].get('ACID', '')}"
+                    f" fss:{plot['380'].get('FSS', 0)}"
+                    f" gsp:{plot['380'].get('GSP', 0)}"
+                    f" ias:{plot['380'].get('IAS', 0)}"
                 , end='')
 
             if plot.get('390'):
                 print(
-                    f" cs:{plot['390'].get('ADR')}"
-                    f" type:{plot['390'].get('TYPE')}"
-                    f" wtc:{plot['390'].get('WTC')}"
-                    f" adep:{plot['390'].get('DEP')}"
-                    f" ades:{plot['390'].get('DES')}"
-                    f" cfl:{plot['390'].get('CFL')}"
+                    f" cs:{plot['390'].get('CS', '')}"
+                    f" type:{plot['390'].get('TYPE', '')}"
+                    f" wtc:{plot['390'].get('WTC', '')}"
+                    f" adep:{plot['390'].get('DEP', '')}"
+                    f" ades:{plot['390'].get('DES', '')}"
+                    f" cfl:{plot['390'].get('CFL', 0)}"
                 , end='')
             print('')
 
         data = astfile.read(3)
+
+end = time.perf_counter()
+print(f">>> processing time: {end - start:0.4f} secs.")
