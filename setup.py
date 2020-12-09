c487753385dfa5f20aff7d9c43eafd495b4edeaf
Filip Jonckers <filip@jonckers.org>
2020-12-09 22:35:12 +0100

    bump to v1.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.0.4'
+VERSION = '1.0.5'
 PYTHON_REQUIRES = '>=3.7'
 PACKAGE_NAME = 'asterix4py'
 AUTHOR = 'Filip Jonckers'
